be36a93ea4f1a4c7d54bce5eae660fc2fdfc2275
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2024-11-04 16:36:19 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/ch3_calrender_project/Ch3CalrenderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch3_calrender_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3CalrenderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3CalrenderProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch3_calrender_project/Ch3CalrenderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch3_calrender_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3CalrenderProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
