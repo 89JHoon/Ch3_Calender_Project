2f8bb8c345f3dc5ec6aaf153efeafea202e77dca
cooleHoonst <88084780+89JHoon@users.noreply.github.com>
2024-11-08 06:08:04 +0900

    중복 파일 제거

--- src/main/java/com/example/ch3_calrender_project/controller/ScheduleController.java
+++ src/main/java/com/example/ch3_calrender_project/controller/ScheduleController.java
@@ -3,12 +3,12 @@ package com.example.ch3_calrender_project.controller;
 import com.example.ch3_calrender_project.dto.ScheduleRequestDto;
 import com.example.ch3_calrender_project.dto.ScheduleResponseDto;
 import com.example.ch3_calrender_project.entity.Schedule;
+import com.example.ch3_calrender_project.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import repository.ScheduleRepository;
 
 @RestController
 @RequestMapping("/main")
